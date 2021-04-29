dc2553d359a46ad244203b486f003ce19e35e964
Wirii <32673858+Wirii@users.noreply.github.com>
2021-04-29 22:55:47 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,190 @@
+const colors = {
+	fire: '#f08030',
+	grass: '#78c850',
+	electric: '#f8d030',
+	water: '#6890f0',
+	ground: '#e0c068',
+	rock: '#b8a038',
+	fairy: '#edb4ba',
+	poison: '#a040a0',
+	bug: '#a8b61f',
+	dragon: '#7038f8',
+	psychic: '#f85888',
+	flying: '#a890f0',
+	fighting: '#c03028',
+	normal: '#a8a878',
+  ghost: '#705897',
+  dark: '#705848',
+  steel: '#b8b8d0',
+  ice: '#98d8d8'
+};
+
+let lettersArr = [];
+let guessArr = [];
+let pokeName = "";
+let toGuess = "";
+
+const gameDiv = document.querySelector(".game");
+const startDiv = document.querySelector(".start");
+const messageContainer = document.querySelector(".replay");
+const message = document.querySelector(".msg");
+const pokeArt = document.querySelector(".pkmn-art");
+const pkmnType1 = document.querySelector(".type1");
+const pkmnType2 = document.querySelector(".type2");
+const giveUpBtn = document.querySelector(".give-up");
+const replayBtn = document.querySelector(".again");
+const startBtn = document.querySelector(".start-btn");
+const hiddenName = document.querySelector(".pkmn-guess");
+const usedLetters = document.querySelector(".used");
+
+
+// inits game
+const initGame = async () => {
+  startDiv.style.display = "none";
+  document.querySelector(".loader").style.display = "flex";
+  await fetchData();
+  pokeArt.onload = () => {
+    gameDiv.style.display = "flex";
+    document.querySelector(".loader").style.display = "none";
+  }
+}
+
+// get random pokemon id
+const randomPokemon = () => {
+  let pokeId =  Math.floor((Math.random() * 898) + 1);
+  console.log(pokeId);
+  return pokeId;
+}
+
+// display pokemon img and type, call guess
+const displayPokemon = (pokemon) => {
+  const type1 = pokemon.types[0].type.name;
+  pkmnType1.style.backgroundColor = colors[type1];
+  pkmnType1.innerHTML = `${type1}`;
+  
+  if (pokemon.types[1]) {
+    const type2 = pokemon.types[1].type.name;
+    pkmnType2.style.backgroundColor = colors[type2];
+    pkmnType2.innerHTML = `${type2}`;
+  }
+  
+  const pokeImg = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png`;
+  pokeArt.src = pokeImg;
+  pokeArt.style.filter = "brightness(0)";
+  
+  pokeArt.onload = () => {
+    document.querySelector(".game").style.display = "flex";
+    document.querySelector(".loader").style.display = "none";
+  }
+}
+
+// handles give up action
+const giveUp = () => {
+  giveUpBtn.addEventListener("click", event => {
+    event.preventDefault();
+    messageContainer.style.display = "flex";
+    message.innerHTML = "Yikes, better luck next time!";
+    let correct = pokeName.split("").join(" ");
+    hiddenName.innerHTML = `${correct.toUpperCase()}`;
+    pokeArt.style.filter = "brightness(1)";
+    hiddenName.style.color = "red";
+  })
+}
+
+// handles replay action
+const replay = () => {
+  replayBtn.onclick = (event) => {
+    event.preventDefault();
+    messageContainer.style.display = "none";
+    document.querySelector(".game").style.display = "none";
+    document.querySelector(".loader").style.display = "flex";
+    resetGame();
+    fetchData();
+  }
+}
+ 
+// resets game data
+const resetGame = () => {
+  lettersArr = [];
+  guessArr = [];
+  pokeName = "";
+  toGuess = "";
+  pkmnType1.innerHTML = "";
+  pkmnType2.innerHTML = "";
+  pkmnType1.style.background = "transparent";
+  pkmnType2.style.background = "transparent";
+  usedLetters.innerHTML = "";
+  pokeArt.src = "";
+  pokeArt.style.filter = "brightness(1)";
+  hiddenName.style.color = "black";
+}
+
+const welcomeScreen = () => {
+  gameDiv.style.display = "none";
+  startDiv.style.display = "flex";
+  document.querySelector(".loader").style.display = "none";
+  startBtn.addEventListener("click", initGame);
+}
+
+// handles listening to user input
+const letterListener = (e) => {
+  if(e.keyCode >= 65 && e.keyCode <= 90) {
+    let letter = e.key;
+
+    // change "_" to letter if correct
+    if (pokeName.indexOf(letter) != -1) {
+      for (let j = 0; j < guessArr.length; j++) {
+        if(pokeName[j] === letter) {
+          guessArr[j] = letter;
+        }
+      }
+      toGuess = guessArr.join(" ");
+      hiddenName.innerHTML = `${toGuess.toUpperCase()}`;
+    } else {
+      // push letter to wrong letters if not there already
+      if (!lettersArr.includes(letter)) {
+        lettersArr.push(letter);        
+      }       
+      usedLetters.innerHTML = `${lettersArr.join(" ").toUpperCase()}`;
+      usedLetters.style.color = "red";
+    }
+
+    console.log(toGuess + ", " + pokeName);
+
+    // check if guessed
+    if (toGuess.replaceAll(" ", "") === pokeName) {      
+      message.innerHTML = "Congrats! You guessed it right!";
+      messageContainer.style.display = "flex";
+      pokeArt.style.filter = "brightness(1)";
+      hiddenName.style.color = "green";
+    }
+  }
+}
+
+const fetchData = async () => {
+  let pokeId = randomPokemon();
+  let url = `https://pokeapi.co/api/v2/pokemon/${pokeId}`;
+  let result = await fetch(url);
+  let pokemon = await result.json();
+  
+  displayPokemon(pokemon);
+  
+  pokeName = pokemon.species.name;
+  console.log(pokeName);
+  for (let i = 0; i < pokeName.length; i++) {
+    if(pokeName[i] == "-") {
+      toGuess = toGuess + "- ";
+    } else {
+      toGuess = toGuess + "_ ";     
+    }
+  }
+  
+  guessArr = toGuess.split(" ");
+  hiddenName.innerHTML = `${toGuess}`;
+  
+  window.addEventListener("keydown", letterListener);
+};
+
+welcomeScreen();
+giveUp();
+replay();
